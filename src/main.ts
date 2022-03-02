5d19f24556742e9f377c9849ee993869e9e73d24
masashi kishimoto <drehbleistift@gmail.com>
2022-03-02 16:49:00 +0900

    つくった

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+const SHEET_ID = "14rxmcAjxXvkd_r332SzjndSQnGMCyGWWorHN_KdsRPg";
+const SHEET_NAME = "お気に入りレシピ";
+
+type Recipe = {
+  url: string;
+  title: string;
+  imageUrl: string;
+};
+
+type Recipes = Recipe[];
+
+const doGet = (e) => {
+  const rows = fetchRecipes();
+  const recipes = rows.map((row) => array2Obj(row));
+  return encode(recipes);
+};
+
+const fetchRecipes = (): string[][] => {
+  const sheet = SpreadsheetApp.openById(SHEET_ID).getSheetByName(SHEET_NAME);
+  const lastRow = sheet.getLastRow();
+  const data = sheet.getRange(2, 1, lastRow - 1, 7).getValues();
+  return data;
+};
+
+const array2Obj = (data: string[]): Recipe => {
+  const [url, title, imageUrl] = data;
+  return { url, title, imageUrl };
+};
+
+const encode = (data: Recipes): GoogleAppsScript.Content.TextOutput => {
+  const json = JSON.stringify(data);
+  const output = ContentService.createTextOutput(json);
+  return output.setMimeType(ContentService.MimeType.JSON);
+};
